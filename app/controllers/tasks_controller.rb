e8747d7f5f082cc79d3adfe399f15af042748ddd
Ichigo <ichigo@web-freework.com>
2020-11-21 13:29:40 +0900

    ログインフォーム用のコントローラー

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,42 @@
 class TasksController < ApplicationController
   def index
-    @tasks=Task.all
+    @tasks = Task.all
   end
 
   def show
-    @task=Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def new
-    @task=Task.new
+    @task = Task.new
   end
 
   def create
-    task=Task.new(task_param)
-    task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    @task = Task.new(task_param)
+    if @task.save
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_param)
+    redirect_to tasks_url, notice: "タスク「#{task.name}」更新しました。"
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
   def task_param
-    params.require(:task).permit(:name,:description)
+    params.require(:task).permit(:name, :description)
   end
 end
