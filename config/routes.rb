52a0771e8724b2eca56b11bfd6188bb3b7409653
Ichigo <ichigo@web-freework.com>
2020-11-28 10:01:50 +0900

    mail

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users do
       post :confirm,action: :confirm_new,on: :new
+      # post :confirm,action: :confirm_new,on: :edit
     end
   end
   root to: 'tasks#index'
